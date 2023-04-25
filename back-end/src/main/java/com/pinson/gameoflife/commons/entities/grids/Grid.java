79768315655d155bcb9a6dd4551c7b65bdde7278
Julien Pinson <pinsonj01@gmail.com>
2023-04-25 23:51:03 +0200

    Implemented MatrixArrayList into Grid

--- back-end/src/main/java/com/pinson/gameoflife/commons/entities/grids/Grid.java
+++ back-end/src/main/java/com/pinson/gameoflife/commons/entities/grids/Grid.java
@@ -1,72 +1,48 @@
 package com.pinson.gameoflife.commons.entities.grids;
 
 import com.pinson.gameoflife.commons.entities.cells.ICell;
+import com.pinson.gameoflife.commons.helpers.lists.matrixArrayLists.IMatrixArrayList;
+import com.pinson.gameoflife.commons.helpers.lists.matrixArrayLists.exceptions.MatrixIndexOutOfBoundsException;
 
 import java.util.ArrayList;
 
 public class Grid<T extends ICell> implements IGrid<T> {
-    /*private ArrayList<ArrayList<T>> cells;
 
-    private int columns;
-    private int rows;
+    private IMatrixArrayList<T> cells;
 
-
-    public Grid(int rows, int columns) {
-        this.rows = rows;
-        this.columns = columns;
-
-        this.cells = new ArrayList<>();
-        for (int i = 0; i < rows; ++i) {
-            ArrayList<T> row = new ArrayList<>();
-            for (int j = 0; j < columns; ++j) {
-                row.add(null);
-            }
-
-            this.cells.add(row);
-        }
+    @Override
+    public int getRows() {
+        return this.cells.getRows();
     }
 
     @Override
     public int getColumns() {
-        return columns;
-    }
-
-    protected void setColumns(int columns) {
-        this.columns = columns;
+        return this.cells.getColumns();
     }
 
     @Override
-    public int getRows() {
-        return rows;
-    }
-
-    protected void setRows(int rows) {
-        this.rows = rows;
-    }
-
-    @Override
-    public ArrayList<ArrayList<T>> getCells() {
-        // Todo : Return a copy of the tiles
-
+    public IMatrixArrayList<T> getCells() {
         return this.cells;
     }
 
     @Override
-    public T getCellAt(int row, int column) {
-        if (row < 0 || row >= this.rows)
-            throw new GridIndexOutOfBoundsException("Row must be within grid bounds.");
-        if (column < 0 || column >= this.columns)
-            throw new GridIndexOutOfBoundsException("Column must be within grid bounds.");
-
-        return this.tiles.get(row).get(column);
+    public T getCellAt(int row, int column) throws MatrixIndexOutOfBoundsException {
+        return this.cells.get(row, column);
     }
 
     @Override
-    public IGrid<T> setCellAt(int row, int column, T cell) {
-        this.cells[row][column] = cell;
+    public IGrid<T> setCellAt(int row, int column, T cell) throws MatrixIndexOutOfBoundsException {
+        this.cells.set(row, column, cell);
+
         return this;
     }
 
-    */
+    // todo: getCircularNeighbours
+    // todo: getSquaredNeighbours using getRectangularNeighbours
+    // todo: getPerpendicularNeighbours (or better name for typical cross) (+)
+    // todo: getDiagonalNeighbours (X)
+    // todo: getRectangularNeighbours
+    // todo: getTriangularNeighbours
+    // todo: getHexagonalNeighbours
 
 }
--- back-end/src/main/java/com/pinson/gameoflife/commons/entities/grids/IGrid.java
+++ back-end/src/main/java/com/pinson/gameoflife/commons/entities/grids/IGrid.java
@@ -1,17 +1,21 @@
 package com.pinson.gameoflife.commons.entities.grids;
 
 import com.pinson.gameoflife.commons.entities.cells.ICell;
+import com.pinson.gameoflife.commons.helpers.lists.matrixArrayLists.IMatrixArrayList;
+import com.pinson.gameoflife.commons.helpers.lists.matrixArrayLists.exceptions.MatrixIndexOutOfBoundsException;
 
 import java.util.ArrayList;
 
 public interface IGrid<T extends ICell> {
-    /*int getRows();
+
+    int getRows();
+
     int getColumns();
-    ArrayList<ArrayList<T>> getCells();
 
-    T getCellAt(int row, int column);
-    IGrid<T> setCellAt(int row, int column, T cell);
+    IMatrixArrayList<T> getCells();
 
+    T getCellAt(int row, int column) throws MatrixIndexOutOfBoundsException;
 
-     */
+    IGrid<T> setCellAt(int row, int column, T cell) throws MatrixIndexOutOfBoundsException;
+    
 }
