d9b41e6e85c96e060af8af2ab61411ad174db3fe
Julien Pinson <pinsonj01@gmail.com>
2023-07-19 22:23:22 +0200

    Wrote the lobby controller methods, still need tests.

--- back-end/src/main/java/com/pinson/othello/lobbies/OthelloLobbyController.java
+++ back-end/src/main/java/com/pinson/othello/lobbies/OthelloLobbyController.java
@@ -1,37 +1,76 @@
 package com.pinson.othello.lobbies;
 
+import com.pinson.othello.commons.exceptions.InvalidNumberOfPlayersException;
+import com.pinson.othello.games.OthelloGame;
+import com.pinson.othello.games.OthelloGameService;
+import com.pinson.othello.lobbies.dtos.responses.OthelloLobbyResponse;
+import com.pinson.othello.lobbies.dtos.responses.OthelloLobbyResponseFactory;
+import com.pinson.othello.lobbies.exceptions.LobbyNotFoundException;
+import com.pinson.othello.players.OthelloPlayer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/lobbies")
 public class OthelloLobbyController {
     private final OthelloLobbyService othelloLobbyService;
+    private final OthelloGameService othelloGameService;
+    private final OthelloLobbyResponseFactory othelloLobbyResponseFactory;
 
     @Autowired
     public OthelloLobbyController(
-        OthelloLobbyService othelloLobbyService
+        OthelloLobbyService othelloLobbyService,
+        OthelloGameService othelloGameService,
+        OthelloLobbyResponseFactory othelloLobbyResponseFactory
     ) {
         this.othelloLobbyService = othelloLobbyService;
+        this.othelloGameService = othelloGameService;
+        this.othelloLobbyResponseFactory = othelloLobbyResponseFactory;
     }
 
-    /*
-    @PostMapping("/classic/join")
-    public ResponseEntity<> joinClassic(
-    ) {
+    @GetMapping("/{id}")
+    public ResponseEntity<OthelloLobbyResponse> getLobbyById(@PathVariable Long id) {
+        try {
+            OthelloLobby lobby = this.othelloLobbyService.getLobbyById(id);
+            return ResponseEntity.ok(
+                this.othelloLobbyResponseFactory.create(lobby)
+            );
+        } catch (LobbyNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
+    @PostMapping("/classic/join")
+    public ResponseEntity<OthelloLobbyResponse> joinClassic() {
+        // must be tested :
+        OthelloPlayer player = (OthelloPlayer) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        OthelloLobby lobby = this.othelloLobbyService.addPlayerToRandomClassicLobby(player);
+
+        if (lobby.isFull()) {
+            // start game
+            try {
+                OthelloGame game = this.othelloGameService.startClassicGame(lobby);
+                lobby = game.getLobby();
+            } catch (InvalidNumberOfPlayersException e) {
+                // this should never happen.
+            }
+        }
+
+        return ResponseEntity.ok(
+            this.othelloLobbyResponseFactory.create(lobby)
+        );
     }
 
     @PostMapping("/leave/{id}")
-    public ResponseEntity<> leave(
+    public ResponseEntity<Void> leave(@PathVariable Long id) {
+        // get user
+        OthelloPlayer player = null;
+        this.othelloLobbyService.removePlayerFromLobby(id, player);
 
-    ) {
-
+        return ResponseEntity.ok().build();
     }
-     */
 
 }
--- back-end/src/main/java/com/pinson/othello/lobbies/OthelloLobbyService.java
+++ back-end/src/main/java/com/pinson/othello/lobbies/OthelloLobbyService.java
@@ -33,7 +33,7 @@ public class OthelloLobbyService {
         this.othelloLobbyRepository.deleteById(id);
     }
 
-    public OthelloLobby addPlayerToRandomLobby(OthelloPlayer player, int maxPlayers) throws FullLobbyException, PlayerAlreadyInLobbyException, NonPositiveValueException, NonEvenNumberException {
+    public OthelloLobby addPlayerToRandomLobby(OthelloPlayer player, int maxPlayers) throws NonPositiveValueException, NonEvenNumberException {
         if (maxPlayers <= 0)
             throw new NonPositiveValueException("Max players must be positive");
         if (maxPlayers % 2 != 0)
@@ -57,9 +57,24 @@ public class OthelloLobbyService {
                 return newLobby;
             });
 
-        lobby.addPlayer(player);
+        try {
+            lobby.addPlayer(player);
+        } catch (PlayerAlreadyInLobbyException | FullLobbyException e) {
+            // This should never happen.
+            throw new RuntimeException(e);
+        }
 
         return othelloLobbyRepository.save(lobby);
+    }
+
+    public OthelloLobby addPlayerToRandomClassicLobby(OthelloPlayer player) {
+        try {
+            return this.addPlayerToRandomLobby(player, 2);
+        } catch (NonPositiveValueException | NonEvenNumberException e) {
+            // This should never happen.
+        }
+
+        return null;
     }
 
     public List<OthelloLobby> getAllLobbiesByMaxPlayers(Integer maxPlayers) {
